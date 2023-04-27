a3a763d1c432057b1567281e6583a1c50e1585e2
Andrew Darts <mandrewdarts@gmail.com>
2023-04-27 09:36:45 -0500

    rm dist and update vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/global-custom-event',
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
